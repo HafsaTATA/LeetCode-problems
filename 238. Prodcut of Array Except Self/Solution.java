b596066267febf7c16200c14bbe81718985a6791
HafsaTATA <120058921+HafsaTATA@users.noreply.github.com>
2024-03-15 22:05:59 +0000

    Create Solution.java

---
+++ 238. Prodcut of Array Except Self/Solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int prod = 1;
+        int zeroExist = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                prod *= num;
+            } else {
+                zeroExist++;
+            }
+        }
+
+        if (zeroExist >= 2) {
+            int[] result = new int[nums.length];
+            Arrays.fill(result, 0);
+            return result;
+        } else if (zeroExist == 1) {
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != 0) {
+                    nums[i] = 0;
+                } else {
+                    nums[i] = prod;
+                }
+            }
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                nums[i] = prod / nums[i];
+            }
+        }
+
+        return nums;
+    }
+}
